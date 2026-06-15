4da7ae1d48b852ecdf4b3f252fe4c5db75e6e233
MADDOS98 <145375158+MADDOS98@users.noreply.github.com>
2026-06-15 12:54:01 +0300

    fix: Remove background color from profile edit forms for consistent styling

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,17 +3,17 @@
     <div class="space-y-4 max-w-2xl">
 
         {{-- Update Profile Info --}}
-        <div class="bg-panel border border-border rounded-lg p-6">
+        <div class="border border-border rounded-lg p-6">
             @include('profile.partials.update-profile-information-form')
         </div>
 
         {{-- Update Password --}}
-        <div class="bg-panel border border-border rounded-lg p-6">
+        <div class="border border-border rounded-lg p-6">
             @include('profile.partials.update-password-form')
         </div>
 
         {{-- Delete Account --}}
-        <div class="bg-panel border border-border rounded-lg p-6">
+        <div class="border border-border rounded-lg p-6">
             @include('profile.partials.delete-user-form')
         </div>
 
